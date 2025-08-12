1dd7c9ac5a2b391abe6e71e9a512130a8144b381
Aleksandr Kostromin <ulis89@yandex.ru>
2025-08-13 02:18:43 +0300

    upd locators

--- locators.py
+++ locators.py
@@ -5,24 +5,26 @@ class Locators:
     # Главная страница
     LOGIN_REG_BUTTON = (By.XPATH, '//button[contains(text(), "Вход и регистрация")]')
     POST_AD_BUTTON = (By.XPATH, '//button[contains(text(), "Разместить объявление")]')
-    USER_AVATAR = (By.CLASS_NAME, 'user-avatar')
-    USER_NAME = (By.CLASS_NAME, 'user-name')
+    USER_AVATAR = (By.CLASS_NAME, 'circleSmall')
+    USER_NAME = (By.CSS_SELECTOR, "h3.profileText.name")
     LOGOUT_BUTTON = (By.XPATH, '//button[contains(text(), "Выйти")]')
     PROFILE_LINK = (By.LINK_TEXT, 'Профиль')
 
-    # Форма авторизации
-    EMAIL_INPUT = (By.ID, 'email')
-    PASSWORD_INPUT = (By.ID, 'password')
+    # Форма регистрации
+    EMAIL_INPUT = (By.CSS_SELECTOR, "input[name='email']")
+    PASSWORD_INPUT = (By.CSS_SELECTOR, "input[name='password']")
+    CONFIRM_PASSWORD_INPUT = (By.CSS_SELECTOR, "input[name='submitPassword']")
     LOGIN_BUTTON = (By.XPATH, '//button[contains(text(), "Войти")]')
 
     # Форма регистрации
     NO_ACCOUNT_BUTTON = (By.XPATH, '//button[contains(text(), "Нет аккаунта")]')
     NAME_INPUT = (By.ID, 'name')
-    CONFIRM_PASSWORD_INPUT = (By.ID, 'confirmPassword')
+    # CONFIRM_PASSWORD_INPUT = (By.ID, 'confirmPassword')
     CREATE_ACCOUNT_BUTTON = (By.XPATH, '//button[contains(text(), "Создать аккаунт")]')
 
     # Ошибки
-    ERROR_MESSAGE_UNDER_EMAIL = (By.XPATH, '//input[@id="email"]/following-sibling::span[contains(text(), "Ошибка")]')
+    ERROR_MESSAGE_UNDER_EMAIL = (By.CLASS_NAME, 'input_span__yWPqB')
+    ERROR_ELEMENT = (By.CLASS_NAME, "input_inputError__fLUP9")
 
     # Модальное окно
     MODAL_AUTH_REQUIRED = (By.XPATH, '//h2[contains(text(), "Чтобы разместить объявление, авторизуйтесь")]')
