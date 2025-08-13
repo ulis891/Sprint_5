9256bb54b9f7cf4ae03689a48d3f03e4c0b6c082
Aleksandr Kostromin <ulis89@yandex.ru>
2025-08-13 19:35:17 +0300

    add create_email fixtire

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 import pytest
 from selenium import webdriver
+import time
+
+
+@pytest.fixture
+def base_url():
+    return "https://qa-desk.stand.praktikum-services.ru/"
 
 
 @pytest.fixture
@@ -11,5 +17,5 @@ def driver():
 
 
 @pytest.fixture
-def base_url():
-    return "https://qa-desk.stand.praktikum-services.ru/"
+def create_email():
+    return f"test_{int(time.time())}@praktikum.test"
