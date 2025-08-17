ad740053783ea6264cbefa4baa3c5e3a5b261e72
Aleksandr Kostromin <ulis89@yandex.ru>
2025-08-17 13:29:12 +0300

    upd locators.py

--- locators.py
+++ locators.py
@@ -23,16 +23,22 @@ class Locators:
     ERROR_ELEMENT = (By.CLASS_NAME, "input_inputError__fLUP9")
 
     # Модальное окно
-    MODAL_AUTH_REQUIRED = (By.XPATH, '//h2[contains(text(), "Чтобы разместить объявление, авторизуйтесь")]')
+    MODAL_AUTH_REQUIRED = (By.XPATH, '//h1[contains(text(), "Чтобы разместить объявление, авторизуйтесь")]')
 
     # Форма создания объявления
     AD_TITLE_INPUT = (By.CSS_SELECTOR, "input[name='name']")
-    AD_DESCRIPTION_INPUT = (By.CSS_SELECTOR, "input[name='description']")
+    AD_DESCRIPTION_INPUT = (By.CSS_SELECTOR, "textarea[name='description']")
     AD_PRICE_INPUT = (By.CSS_SELECTOR, "input[name='price']")
-    CATEGORY_DROPDOWN = (By.ID, 'category')
-    CITY_DROPDOWN = (By.ID, 'city')
-    CONDITION_RADIO_NEW = (By.XPATH, '//input[@type="radio" and @value="new"]')
-    PUBLISH_BUTTON = (By.XPATH, '//button[contains(text(), "Опубликовать")]')
+    CATEGORY_DROPDOWN = (By.XPATH, "//input[@name='category']/following-sibling::button")
+    CATEGORY_OPTION_AUTO = (By.XPATH, "//button[span[text()='Авто']]")
+    CITY_DROPDOWN = (By.XPATH, "//input[@name='city']/following-sibling::button")
+    CITY_OPTION_MOSCOW = (By.XPATH, "//button[span[text()='Москва']]")
+    NEW_CONDITION_RADIO = (By.XPATH, "//input[@name='condition' and @value='Новый']")
+    USED_CONDITION_RADIO = (By.XPATH, "//input[@name='condition' and @value='Б/У']")
+    PUBLISH_BUTTON = (By.XPATH, "//button[text()='Опубликовать']")
 
     # Мои объявления
-    MY_ADS_SECTION = (By.XPATH, '//h3[contains(text(), "Мои объявления")]')
+    AD_CARD = (By.XPATH, '//div[@class="card"]')
+    AD_CARD_TITLE = (By.CSS_SELECTOR, '.card .about h2')
+    AD_CARD_CITY = (By.CSS_SELECTOR, '.card .about h3')
+    AD_CARD_PRICE = (By.CSS_SELECTOR, "div.card div.price h2.h2")
