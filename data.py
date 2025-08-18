61f7b15e80f30f7e172312f9c5ea2a0994eecc42
Aleksandr Kostromin <ulis89@yandex.ru>
2025-08-18 18:47:59 +0300

    del unused pasword

--- data.py
+++ data.py
@@ -5,7 +5,6 @@ class TestData:
     TEST_USER_EMAIL = "test_user@example.com"
     TEST_USER_PASSWORD = "Qwerty123!"
     INVALID_EMAIL = "invalid_email"
-    PASSWORD = "123"
 
     AD_TITLE = 'LADA'
     AD_CITY = 'Москва'
