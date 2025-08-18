dc4cc10be332b8939bfce14d29d5db64180e9121
ulis891 <99750497+ulis891@users.noreply.github.com>
2025-08-18 17:39:09 +0300

    fix test_ad_creation

--- tests/test_ad_creation.py
+++ tests/test_ad_creation.py
@@ -1,20 +1,23 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import Locators as L
+from data import TestData as D
+import helpers
 
 
-class TesttestAdCreation:
-    def test_ad_creation_by_authorized_user_success(self, driver, base_url, create_email):
-        email = create_email
-        password = "password123"
+class TestAdCreation:
+    def test_ad_creation_by_authorized_user_success(self, driver):
+        email,password = helpers.get_new_user()
 
-        ad = {'title': 'LADA',
-              'city': 'Москва',
-              'description': 'Седан, баклажан, 2 ядра, 2 колеса',
-              'price': '696'}
+        ad = {
+            'title': D.AD_TITLE,
+            'city': D.AD_CITY,
+            'description': D.AD_DESCRIPTION,
+            'price': D.AD_PRICE
+            }
 
-        driver.get(base_url)
-        wait = WebDriverWait(driver, 10)
+        driver.get(D.BASE_URL)
+        wait = WebDriverWait(driver, D.WAIT_TIME)
         wait.until(EC.element_to_be_clickable(L.LOGIN_REG_BUTTON)).click()
         wait.until(EC.element_to_be_clickable(L.NO_ACCOUNT_BUTTON)).click()
         driver.find_element(*L.EMAIL_INPUT).send_keys(email)
@@ -26,7 +29,7 @@ class TesttestAdCreation:
         wait.until(EC.visibility_of_element_located(L.USER_AVATAR))
 
         wait.until(EC.element_to_be_clickable(L.POST_AD_BUTTON)).click()
-        wait.until(EC.visibility_of_element_located(L.AD_TITLE_INPUT)).send_keys(ad['title'])
+        wait.until(EC.visibility_of_element_located(L.AD_TITLE_INPUT)).send_keys(D.AD_TITLE)
 
         driver.find_element(*L.CATEGORY_DROPDOWN).click()
         driver.find_element(*L.CATEGORY_OPTION_AUTO).click()
@@ -37,11 +40,11 @@ class TesttestAdCreation:
         driver.find_element(*L.CITY_DROPDOWN).click()
         driver.find_element(*L.CITY_OPTION_MOSCOW).click()
 
-        driver.find_element(*L.AD_DESCRIPTION_INPUT).send_keys(ad['description'])
-        driver.find_element(*L.AD_PRICE_INPUT).send_keys(ad['price'])
+        driver.find_element(*L.AD_DESCRIPTION_INPUT).send_keys(D.AD_DESCRIPTION)
+        driver.find_element(*L.AD_PRICE_INPUT).send_keys(D.AD_PRICE)
         driver.find_element(*L.PUBLISH_BUTTON).click()
 
-        wait.until(EC.url_to_be(base_url))
+        wait.until(EC.url_to_be(D.BASE_URL))
         wait.until(EC.element_to_be_clickable(L.USER_AVATAR)).click()
         wait.until(EC.visibility_of_element_located(L.AD_CARD))
 
@@ -58,10 +61,10 @@ class TesttestAdCreation:
         for parameter in actual_ad:
             assert ad[parameter] == actual_ad[parameter]
 
-    def test_create_ad_not_logged_in(self, driver, base_url):
+    def test_create_ad_not_logged_in(self, driver):
         expected_text = 'Чтобы разместить объявление, авторизуйтесь'
-        driver.get(base_url)
-        wait = WebDriverWait(driver, 10)
+        driver.get(D.BASE_URL)
+        wait = WebDriverWait(driver, D.WAIT_TIME)
         wait.until(EC.element_to_be_clickable(L.POST_AD_BUTTON)).click()
         actual_text = wait.until(EC.visibility_of_element_located(L.MODAL_AUTH_REQUIRED)).text
         assert expected_text == actual_text
