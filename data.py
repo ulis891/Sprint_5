9186afe1c33bae8f086695e8a15f881c2e0a0469
ulis891 <99750497+ulis891@users.noreply.github.com>
2025-08-18 17:03:33 +0300

    add data.py

---
+++ data.py
@@ -0,0 +1,12 @@
+class TestData:
+    BASE_URL = "https://qa-desk.stand.praktikum-services.ru/"
+
+    VALID_EMAIL = "test_user@example.com"
+    VALID_PASSWORD = "Qwerty123!"
+    INVALID_EMAIL = "invalid_email"
+    PASSWORD = "123"
+
+    AD_TITLE =  'LADA',
+    AD_CITY: 'Москва'
+    AD_DESCRIPTION = 'Седан, баклажан, 2 ядра, 2 колеса',
+    AD_PRICE = '696'
