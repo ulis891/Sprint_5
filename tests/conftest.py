45b34610866fad933405132caf0b2809928b6879
ulis891 <99750497+ulis891@users.noreply.github.com>
2025-08-18 17:15:14 +0300

    upd conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,5 @@
 import pytest
 from selenium import webdriver
-import time
-
-
-@pytest.fixture
-def base_url():
-    return "https://qa-desk.stand.praktikum-services.ru/"
 
 
 @pytest.fixture
@@ -14,8 +8,3 @@ def driver():
     driver.maximize_window()
     yield driver
     driver.quit()
-
-
-@pytest.fixture
-def create_email():
-    return f"test_{int(time.time())}@praktikum.test"
