bf9be4d6ef0d08b722f96205205e78329aed931f
Aleksandr Kostromin <ulis89@yandex.ru>
2025-08-18 18:27:40 +0300

    fix data.py

--- data.py
+++ data.py
@@ -2,12 +2,12 @@ class TestData:
     BASE_URL = "https://qa-desk.stand.praktikum-services.ru/"
     WAIT_TIME = 10
 
-    VALID_EMAIL = "test_user@example.com"
-    VALID_PASSWORD = "Qwerty123!"
+    TEST_USER_EMAIL = "test_user@example.com"
+    TEST_USER_PASSWORD = "Qwerty123!"
     INVALID_EMAIL = "invalid_email"
     PASSWORD = "123"
 
-    AD_TITLE =  'LADA',
-    AD_CITY: 'Москва'
+    AD_TITLE = 'LADA'
+    AD_CITY = 'Москва'
     AD_DESCRIPTION = 'Седан, баклажан, 2 ядра, 2 колеса',
     AD_PRICE = '696'
